cf1bcda7303280824a14735aea2974fc02d7b5b1
codot-spec <exform.arc@gmail.com>
2024-11-17 22:25:18 +0530

    update3

--- dom/FruitShop.js
+++ dom/FruitShop.js
@@ -1,8 +1,33 @@
-// Write your code below:
-const fruits = document.getElementsByTagName('li');
-fruits[4].style.color = 'blue';
+// Write the code as shown in the video below:
 
-// Make all elements bold
-for (let i = 0; i < fruits.length; i++) {
-  fruits[i].style.fontStyle = 'italic';
+// Write answer to the questions asked below:
+const mainHeading =document.querySelector('#main-heading');
+mainHeading.style.textAlign='right';
+
+const basketHeading= document.querySelector('#basket-heading');
+basketHeading.style.color ='brown';
+
+const fruits= document.querySelector('.fruits');
+fruits.style.backgroundColor='grey';
+fruits.style.padding='30px';
+fruits.style.margin='30px';
+fruits.style.width='50%';
+fruits.style.borderRadius ='5px';
+fruits.style.listStyleType ='none';
+
+const basketsHeading = document.querySelector('h2');
+basketsHeading.style.marginLeft ='30px';
+
+const fruitItem=document.querySelectorAll('.fruit');
+for (let i=0; i<fruitItem.length;i++){
+  fruitItem[i].style.padding='10px';
+fruitItem[i].style.borderRadius ='5px';
+fruitItem[i].style.margin='10px';
+  fruitItem[i].style.backgroundColor='lightgrey';
+}
+
+const evenfruitItem=document.querySelectorAll('.fruit:nth-child(even)');
+for (let i=0; i<evenfruitItem.length;i++){
+  evenfruitItem[i].style.backgroundColor='brown';
+  evenfruitItem[i].style.color='white';
 }
