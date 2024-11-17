a1acd62259af3dd2fc9e061d514a80682cbbaf5d
codot-spec <exform.arc@gmail.com>
2024-11-17 22:27:36 +0530

    update4

--- dom/FruitShop.js
+++ dom/FruitShop.js
@@ -1,33 +1,13 @@
-// Write the code as shown in the video below:
 
-// Write answer to the questions asked below:
-const mainHeading =document.querySelector('#main-heading');
-mainHeading.style.textAlign='right';
 
-const basketHeading= document.querySelector('#basket-heading');
-basketHeading.style.color ='brown';
+const headerDiv = document.getElementById('header');
+const subHeading = document.createElement('h3');
+subHeading.textContent = 'Buy high quality organic fruits online';
+subHeading.style.fontStyle = 'italic';
+headerDiv.appendChild(subHeading);
 
-const fruits= document.querySelector('.fruits');
-fruits.style.backgroundColor='grey';
-fruits.style.padding='30px';
-fruits.style.margin='30px';
-fruits.style.width='50%';
-fruits.style.borderRadius ='5px';
-fruits.style.listStyleType ='none';
-
-const basketsHeading = document.querySelector('h2');
-basketsHeading.style.marginLeft ='30px';
-
-const fruitItem=document.querySelectorAll('.fruit');
-for (let i=0; i<fruitItem.length;i++){
-  fruitItem[i].style.padding='10px';
-fruitItem[i].style.borderRadius ='5px';
-fruitItem[i].style.margin='10px';
-  fruitItem[i].style.backgroundColor='lightgrey';
-}
-
-const evenfruitItem=document.querySelectorAll('.fruit:nth-child(even)');
-for (let i=0; i<evenfruitItem.length;i++){
-  evenfruitItem[i].style.backgroundColor='brown';
-  evenfruitItem[i].style.color='white';
-}
+const secondDiv = document.querySelector('div:nth-of-type(2)');
+const para = document.createElement('p');
+para.textContent = 'Total fruits: 4';
+para.id = 'fruits-total';
+secondDiv.insertBefore(para, secondDiv.firstChild);
